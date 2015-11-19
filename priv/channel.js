53bd4c84444fd3c8dac923114f8f7418ab392897
Gustaf Sjöberg <gs@pipsq.com>
2015-11-19 22:18:57 +0100

    Added send function.

--- priv/channel.js
+++ priv/channel.js
@@ -1,5 +1,8 @@
 var Channel = {
     emit: function(path, data) {
         return external.run('hydna', ['emit', path, data]);
+    },
+    send: function(path, data) {
+        return external.run('hydna', ['send', path, data]);
     }
 };
--- src/handlers/erlang_v8_hydna.erl
+++ src/handlers/erlang_v8_hydna.erl
@@ -4,4 +4,8 @@
 
 run([<<"emit">>, Path, Data], #{ domain_name := DomainName }) ->
     hydna_domain:emit_to_channel(DomainName, Path, Data),
+    {ok, <<>>};
+
+run([<<"send">>, Path, Data], #{ domain_name := DomainName }) ->
+    hydna_domain:send_to_channel(DomainName, Path, 1, 0, Data),
     {ok, <<>>}.
